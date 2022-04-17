0f7cdaf3f914c978793448ed1af04c989e4b2a58
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-04-17 16:10:34 +0530

    final tested working todo list
    final tested working todo list with feature add ,delele ,edit

---
+++ App.js
@@ -0,0 +1,15 @@
+// import AxiosGet from "./components/AxiosMethods/AxiosGet";
+// import AxiosPostClass from "./components/AxiosMethods/AxiosPostClass";
+import TodoList from "./components/TodoAssignment/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AxiosGet /> */}
+      {/* <AxiosPostClass /> */}
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
